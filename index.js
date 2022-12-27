7994582f2ae12496972571918d3945efc1c67de6
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-12-27 13:00:15 +0200

    Added fixes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
-console.log(array);
 
 function removeElement(array, item) {
-  array.splice(item-1, 1);
-  return removeElement;
+  return array.filter((elem) => elem !== item);
 }
